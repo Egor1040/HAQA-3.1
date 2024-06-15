b794f80d5c3e6ce6c0dedc1b8a880bdeaf5aaad1
Igor <igor.motriy3@gmail.com>
2024-06-15 19:25:48 +0300

    classwork with ifelse

---
+++ ifelse.js
@@ -0,0 +1,114 @@
+let i = 0;
+
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+
+// console.log('Сума ' + sum)
+
+
+
+
+
+
+
+
+
+// let day = 2;
+// let dayname;
+
+// switch (day) {
+//     case 1:
+//         dayname = "Ponedilok"
+//         break;
+//     case 2:
+//         dayname = "Vivtorok";
+// }
+
+// console.log(dayname);
+
+
+
+
+
+
+
+// let age = 11;
+
+// console.log(age >= 18 ? "доступ довзолено" : "доступ заборонено");
+
+// let time = 11;
+
+// if (time >= 6 && time < 12) {
+//     console.log('Доброго ранку');
+// } else if (time >= 12 && time < 18) {
+//     console.log('Доброго дня');
+// } else if (time >=18 && time < 24) {
+//     console.log('Добрий вечір')
+// } else {
+//     console.log('Ніч');
+// }
+
+
+
+
+
+
+
+
+
+// let a = 59;
+
+// if (a >= 90) {
+//     console.log('A')
+// } else if (a >= 80) {
+//     console.log('B');
+// } else if (a >= 60) {
+//     console.log('C');
+// } else {
+//     console.log('нездав')
+// }
+
+
+
+
+
+
+
+
+
+
+// let number = 0;
+
+// if (number > 0) {
+//     console.log('число додатнє');
+// } else if (number < 0) {
+//         console.log('Число відємне');
+//     } else {
+//         console.log("Число дорівнює 0");
+//     }
+
+
+
+
+
+// let age = 11;
+
+// console.log(age >= 18 ? "доступ довзолено" : "доступ заборонено");
+
+
+// let number = 10;
+// if (number % 2 === 0) {
+//     console.log(number + " Парне");
+// } else {
+//     console.log("непарне");
+// }
+
